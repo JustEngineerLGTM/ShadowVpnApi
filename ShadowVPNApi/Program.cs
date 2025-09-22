d8ddd8412d5f8a3f255e0554f88af2a7460d5ff7
JustEngineer <nbyvalin@mail.ru>
2025-09-22 22:07:45 +0300

    Added docker compose

--- ShadowVPNApi/Program.cs
+++ ShadowVPNApi/Program.cs
@@ -15,27 +15,21 @@ await ServerSetup.EnsureServerConfigAsync();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
-    options.SwaggerDoc("v2", new OpenApiInfo { Title = "ShadowVPN API", Version = "v2" });
+    options.SwaggerDoc("v2", new OpenApiInfo
+    {
+        Title = "ShadowVPN API", Version = "v2"
+    });
 });
-builder.WebHost.UseUrls("http://0.0.0.0:5001");
+
 var app = builder.Build();
 
-// В режиме разработки включаем Swagger UI
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v2/swagger.json", "ShadowVPN API v2");
-        options.RoutePrefix = string.Empty;
-    });
-}
+    options.SwaggerEndpoint("/swagger/v2/swagger.json", "ShadowVPN API v2");
+    options.RoutePrefix = string.Empty;
+});
 
-// Перенаправляем HTTP → HTTPS
 app.UseHttpsRedirection();
-
-// Маппинг маршрутов для VPN‑эндпоинтов
 app.MapVpnEndpoints();
-
-// Запускаем приложение
 app.Run();
