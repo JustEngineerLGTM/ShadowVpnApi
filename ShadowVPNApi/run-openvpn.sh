0b3da6524617c889b3cc5d18f507518a45237543
JustEngineer <nbyvalin@mail.ru>
2025-09-23 15:39:28 +0300

    remove rudiments

--- ShadowVPNApi/run-openvpn.sh
+++ ShadowVPNApi/run-openvpn.sh
@@ -10,9 +10,4 @@ OVPN_NATDEVICE=$(ip route get 8.8.8.8 | awk '{print $5; exit}')
 iptables -t nat -C POSTROUTING -s $OVPN_SERVER -o $OVPN_NATDEVICE -j MASQUERADE 2>/dev/null || \
     iptables -t nat -A POSTROUTING -s $OVPN_SERVER -o $OVPN_NATDEVICE -j MASQUERADE
 
-for i in "${OVPN_ROUTES[@]}"; do
-    iptables -t nat -C POSTROUTING -s "$i" -o $OVPN_NATDEVICE -j MASQUERADE 2>/dev/null || \
-        iptables -t nat -A POSTROUTING -s "$i" -o $OVPN_NATDEVICE -j MASQUERADE
-done
-
 exec /usr/sbin/openvpn --config "$OPENVPN/server/server.conf"
